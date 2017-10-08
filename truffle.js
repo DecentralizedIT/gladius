8979ec782a40190f2ed3890454ba2b9d26648c86
Frank Bonnet <frank@dcorp.it>
2017-10-08 11:21:08 +0200

    Added development config for truffle rollback

--- truffle.js
+++ truffle.js
@@ -1,10 +1,14 @@
 module.exports = {
   networks: {
+    test: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*", // Match any network id
-      gas: 3000000
+      network_id: "*" // Match any network id
     },
     ropsten: {
       host: "localhost",
